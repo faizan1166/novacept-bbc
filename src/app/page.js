b1be34cfedcc0671ba23f08a79467225a7b5a740
faizanwbst <71093180+faizan1166@users.noreply.github.com>
2025-12-11 19:08:24 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="container my-4">
+      <h2>Home - BBC News (POC)</h2>
+
+      <p>This is where your content goes.</p>
+    </main>
+  );
+}
